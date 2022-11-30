61a083c1894948fcfa69299391008003b8bb8096
Andréa Masselot <andrea.nicolas128@gmail.com>
2022-11-30 12:41:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+mongoose.connect(process.env.MONGODB_URI);
+const cors = require("cors");
+
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const usersRoute = require("./routes/users");
+
+app.use(usersRoute);
+
+app.all("*", (req, res) => {
+  res.status(404).json({ error: "Not Found" });
+});
+
+app.listen(process.env.PORT, () => console.log("Server started"));
